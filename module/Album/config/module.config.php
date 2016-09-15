eb540aaee16b671b4019a3c28fb67c43b9609b1b
gtursi <gtursi@cajval.sba.com.ar>
2016-09-15 19:00:00 -0300

    Tutorial Zend Framework 3

---
+++ module/Album/config/module.config.php
@@ -0,0 +1,16 @@
+namespace Album;
+
+use Zend\ServiceManager\Factory\InvokableFactory;
+
+return [
+    'controllers' => [
+        'factories' => [
+            Controller\AlbumController::class => InvokableFactory::class,
+        ],
+    ],
+    'view_manager' => [
+        'template_path_stack' => [
+            'album' => __DIR__ . '/../view',
+        ],
+    ],
+];
